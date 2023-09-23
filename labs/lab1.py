77b6c0706dec5222a19531017e9a5dc049b2fc00
NikitaDresvyannikov <144202100+NikitaDresvyannikov@users.noreply.github.com>
2023-09-23 23:09:15 +0500

    Update lab1.py

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,1 +1,13 @@
+import math
 
+def task1():
+    x = float(input('Произвольное x:'))
+    y = float(input('Произвольное y:'))
+    z = float(input('Произвольное z:'))
+
+    a = (3 + (math.e)**2) / (1+(x)**2 * abs(y - math.tan(z)))
+    b = 1 + abs(y - x) + ((y - x)**2) / 2 + ((x - y)**2) / 3
+
+    print('a =', a)
+    print('b =', b)
+task1()
