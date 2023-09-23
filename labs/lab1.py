84574aac54c1fdeab6a01e8b72f9b84049769db4
NikitaDresvyannikov <144202100+NikitaDresvyannikov@users.noreply.github.com>
2023-09-24 01:38:59 +0500

    Update lab1.py

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,6 +1,7 @@
 import math
 
 def task1():
+    
     x = float(input('Произвольное x:'))
     y = float(input('Произвольное y:'))
     z = float(input('Произвольное z:'))
@@ -10,29 +11,38 @@ def task1():
 
     print('a =', a)
     print('b =', b)
+    
 task1()
 
 def task2():
+    
     a = -2
     b = 5
     c = 3
+    
     def f(x):
-        return (b*x + a)**2 / (c + x**3) + x**4
+        return float((b*x + a)**2 / (c + x**3) + x**4)
     print(f())
+    
 task2()
 
 def task4():
+    
     a = float(input('Произвольное a:'))
     b = float(input('Произвольное b:'))
     c = float(input('Произвольное c:'))
+    
     if c == (a**2 + b**2)**0.5:
         h = (a * b) / c
         print('h=', h)
+        
     else:
         print('Треугольник не существует')
+        
 task4()
 
 def task5():
+    
     x1 = float(input('x1='))
     y1 = float(input('y1='))
     z1 = float(input('z1='))
@@ -42,9 +52,25 @@ def task5():
     x3 = float(input('x3='))
     y3 = float(input('y3='))
     z3 = float(input('z3='))
+    
     F1 = [x1,y1,z1]
     F2 = [x2,y2,z2]
     F3 = [x3,y3,z3]
+    
     F = [x1+x2+x3,y1+y2+y3,z1+z2+z3]
+    
     print('F=', F)
+    
 task5()
+
+def task6():
+    
+    R = float(input('R-радиус окружности:'))
+    a = float(input('a - сторона правильного вписанного многоугольника:'))
+    n = float(input('n - кол-во сторон:'))
+    
+    a2n = math.sqrt(2*R**2-2*R*math.sqrt(R**2-(((a**2) * n)/4)))
+    
+    print('a2n=', a2n)
+    
+task6()
