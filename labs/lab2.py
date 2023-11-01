2e9dfe2f08ce5461f90cdbcf67951dffac5a4e41
NikitaDresvyannikov <144202100+NikitaDresvyannikov@users.noreply.github.com>
2023-11-01 11:00:33 +0500

    Update lab2.py

--- labs/lab2.py
+++ labs/lab2.py
@@ -1,1 +1,30 @@
+def task5():
+    pr = 1
+    cnt = 0
+    while cnt != 4:
+        x = float(input('Введите отрицательное число\n'))
+        if x < 0:
+            pr *= x
+            cnt += 1
+        if x > 0:
+            print('Ошибка!\t')
+        if cnt == 4:
+            if pr < 0:
+                print('Произведение отрицательно\n', pr)
+            if pr > 0:
+                print('Произведение положительно\n', pr)
+
+def task6():
+    january = 
+    february =
+    march =
+    april =
+    may =
+    june =
+    july =
+    august =
+    september =
+    october =
+    november =
+    december =
 
