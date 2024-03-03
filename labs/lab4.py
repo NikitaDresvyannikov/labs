7b7ffe5e347b0e23e24a338b88384bbfce7c6436
NikitaDresvyannikov <144202100+NikitaDresvyannikov@users.noreply.github.com>
2024-03-03 15:09:21 +0500

    Update lab4.py

--- labs/lab4.py
+++ labs/lab4.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from PyQt5.QtWidgets import (QMainWindow, QWidget, QFormLayout, QPushButton, QLabel, QLineEdit, QApplication, QComboBox, QTableWidget, QTableWidgetItem)
+from PyQt5.QtCore import (QSize, Qt)
 
 
 class MainWindow(QMainWindow):
@@ -15,40 +16,42 @@ class MainWindow(QMainWindow):
         central_widget = QWidget()
         layout = QFormLayout()
         central_widget.setLayout(layout)
-        
-        items = {'выше', 'ниже'
-            
-        }
-        table = QTableWidget(self)
-        table.setColumnCount(4)
-        table.setRowCount(1)
-        table.setHorizontalHeaderLabels('k', 'степень x', 'b', 'выше/ниже')
-        table.horizontalHeaderItem(0).setToolTip('k')
-        table.horizontalHeaderItem(1).setToolTip('степень x')
-        table.horizontalHeaderItem(2).setToolTip('b')
-        self.combobox = QComboBox(self, 200)
-        self.combobox.addItems(self.items)
-
-
-
-
 
         layout.addWidget(self.canvas)
         self.setCentralWidget(central_widget)
 
         self.check_point = QPushButton('Проверка точки')
         self.check_point.clicked.connect(self.check_points)
+
         self.clear_button = QPushButton('Очистить график')
         self.clear_button.clicked.connect(self.clear)
+
+        self.add_curve = QPushButton('Добавить кривую')
+        self.add_curve.clicked.connect(self.add_curves)
+
         self.range_label = QLabel('Введите координаты точки:')
         self.range_start_input = QLineEdit()
         self.range_end_input = QLineEdit()
+        self.range_start_input.setFixedSize(25,25)
+        self.range_end_input.setFixedSize(25,25)
+
+        self.table = QTableWidget()
+        self.table.setColumnCount(3)
+        self.table.setRowCount(1)
+        self.table.setHorizontalHeaderLabels(['k', 'n', 'b'])
+        self.table.setHorizontalHeaderItem(0, QTableWidgetItem('k'))
+        self.table.setHorizontalHeaderItem(1, QTableWidgetItem('n'))
+        self.table.setHorizontalHeaderItem(2, QTableWidgetItem('b'))
+
 
         layout.addRow(self.range_label)
         layout.addRow(self.range_start_input, self.range_end_input)
         layout.addWidget(self.check_point)
         layout.addWidget(self.clear_button)
+        layout.addRow(self.table)
+        layout.addWidget(self.add_curve)
 
+        self.section = []
         self.grafik()
 
     def grafik(self):
@@ -78,13 +81,8 @@ class MainWindow(QMainWindow):
         ax = plt.subplot()
 
         def fig1_check(x, y):
-            k = self.table.item().text()
-            n = self.table.item().text()
-            b = self.table.item().text()
-            if
-
             r = 2
-            if (x + 1)**2 + (y-3)**2 <= r**2 and x <= 1:
+            if (x + 1) ** 2 + (y - 3) ** 2 <= r ** 2 and x <= 1:
                 return True
             elif x <= 1 and 3 >= y >= (-4 / 3) * x - 1 and y >= 2 * x - 1:
                 return True
@@ -94,7 +92,7 @@ class MainWindow(QMainWindow):
         def fig2_check(x, y):
 
             r = 2
-            if (x - 5)**2 + y**2 <= r**2 and y < 0:
+            if (x - 5) ** 2 + y ** 2 <= r ** 2 and y < 0:
                 return True
             elif x - 3 >= y >= 0 and y <= -x + 7:
                 return True
@@ -114,6 +112,19 @@ class MainWindow(QMainWindow):
         for ax in self.fig.axes:
             ax.clear()
         self.grafik()
+    
+
+    def add_curves(self):
+        k = self.table.item(0, 0).text()
+        n = self.table.item(0, 1).text()
+        b = self.table.item(0, 2).text()
+        self.section.append([k,n,b])
+        self.centralWidget().layout().itemAt(0).widget().draw()
+
+
+
+
+
 
 
 app = QApplication([])
