3993ea97fec7e81a166b90968ecb98c18e09b4b8
NikitaDresvyannikov <144202100+NikitaDresvyannikov@users.noreply.github.com>
2026-02-14 15:31:27 +0500

    Update VICHMEH(Metod_konechnih_raznostei_1).py

--- VICHMEH(Metod_konechnih_raznostei_1).py
+++ VICHMEH(Metod_konechnih_raznostei_1).py
@@ -6,6 +6,9 @@ def f(x):
 def df(x):
     return (2*np.tan(x))/(np.cos(x))**2
 
+def df2(x):
+    return (4 * (np.sin(x))**2 + 2)/(np.cos(x))**4
+
 x_0 = 1.4
 h = 0.1
 
@@ -15,6 +18,7 @@ for i in range(4):
     y_left = (f(x_0) - f(x_0 - h))/ h
     y_right = (f(x_0) + f(x_0 + h))/ h
     y_central = (f(x_0 + h) - f(x_0 - h))/(2*h)
+    dy2 = (f(x_0 + h) - 2 * f(x_0) + f(x_0 - h))/(h**2)
 
     pogr_left = abs((df(x_0)-y_left)/df(x_0))
     pogr_right = abs((df(x_0)-y_right)/df(x_0))
@@ -27,3 +31,6 @@ for j in range(len(pogr)):
     for k in range(len(pogr[j])):
         print(f'{pogr[j][k]:.4e} ',end='')
     print('\n')
+
+print('Вторая производная = ',dy2)
+
